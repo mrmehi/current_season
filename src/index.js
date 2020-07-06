1b89a132b7e805b9d45c486cc37aa939b69afe9f
Mehemmed Sirmemmedli <mehemmed_1997@list.ru>
2020-07-06 18:02:06 +0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{useState,useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const [lat,setLat] = useState(null)
+    const [errorMessage,seterrorMessage] = useState("")
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+    useEffect(()=>{
+        window.navigator.geolocation.getCurrentPosition(
+            pos => setLat(pos.coords.latitude),
+            err => seterrorMessage(err.message)
+        )
+
+    },[])
+
+    let content 
+
+    if(errorMessage){
+        content = <div>Error:{errorMessage}</div>
+    }else if(lat){
+        content = <SeasonDisplay lat = {lat}/>
+    }else{
+        content = <Spinner message = "Zəhmət olmassa lokasiyanızı müəyyən etməyimizə icazə verəsiniz"/>
+    }
+
+
+    return <div className="border red">{content}</div>
+
+}
+
+
+
+// class App extends React.Component{
+//     constructor(props){
+//         super()
+
+//         this.state = {
+//             lat: "",
+//             errorMessage:"",
+           
+//         }
+       
+//     }
+//     componentDidMount(){
+//         window.navigator.geolocation.getCurrentPosition(
+//             pos => this.setState({lat:pos.coords.latitude}),
+//             err => this.setState({errorMessage:err.message})
+//         )
+//     }
+    
+//     render(){
+
+//         if(this.state.lat && !this.state.errorMessage){
+//             return <SeasonDisplay lat = {this.state.lat}/>
+//         }
+//         else if(this.state.errorMessage && !this.state.lat){
+//             return <div>{this.state.errorMessage}</div>
+//         }
+
+//         return <Spinner message = "Zəhmət olmassa lokasiyanızı müəyyən etməyimizə icazə verəsiniz"/>
+//     }
+// }
+
+
+
+
+
+
+ReactDOM.render(<App/>,document.querySelector('#root'))
